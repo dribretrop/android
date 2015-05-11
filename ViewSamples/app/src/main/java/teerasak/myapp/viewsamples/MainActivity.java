948457f5269bc4e434f02dc23b267b7cfad21c87
dribretrop <porter_bird@hotmail.com>
2015-05-12 01:27:02 +0700

    'ViewSamples'

---
+++ ViewSamples/app/src/main/java/teerasak/myapp/viewsamples/MainActivity.java
@@ -0,0 +1,64 @@
+package teerasak.myapp.viewsamples;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    //define Var
+    public EditText email;
+    public TextView label;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email = (EditText) findViewById(R.id.editText);
+        label = (TextView) findViewById(R.id.textView);
+
+
+    }   //onCreate()
+
+
+    public void changeLabel() {
+        label.setText("Your email is " + email.getText().toString());
+
+    }   //changeLabel()
+
+
+    public void formSubmit(View view) {
+        Toast.makeText(view.getContext(), "Your email is " + email.getText().toString(),Toast.LENGTH_SHORT).show();
+        changeLabel();
+    } // formSubmit()
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
